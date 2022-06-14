04c52ac684094c8b590b0c21ead42281c0b82ddb
arash <arashmd81@gmail.com>
2022-06-14 11:40:52 +0430

    Test template loaded
    (practice of mixing css and js filse via mix.------ in webpakage.mix.js & "npm run dev" command) and use them in a template correctly.

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,24 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+mix
+.styles([
+    'resources/css/base.css',
+    'resources/css/magnific.css',
+    'resources/css/animated-headlines.css',
+    'resources/css/style.css'
+],
+    'public/css/mixedStyles.css'
+)
+.scripts([
+    'resources/js/modernizr.custom.js',
+    'resources/js/jquery.js',
+    'resources/js/isotope.js',
+    'resources/js/magnific.js',
+    'resources/js/animated-headlines.js',
+    'resources/js/waypoints.js',
+    // 'resources/js/ie8.js',
+    'resources/js/init.js',
+],
+    'public/js/mixedScripts.js'
+)
