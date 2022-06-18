1a5098af94bd2607d36c7a5dda909c4049aa0602
arash <arashmd81@gmail.com>
2022-06-18 16:52:24 +0430

    delete unused files(2)

--- resources/views/showCars.blade.php
+++
@@ -1,9 +0,0 @@
-@foreach($cars as $car)
-    company : {{$car['company']}}<br>
-    model : {{$car['model']}}<br>
-    color : {{$car['color']}}<br>
-    price : {{$car['price']}}<br>
-    created_at : {{$car['created_at']}}<br>
-    updated_at: {{$car['updated_at']}}<br>
-    ---------------------------<br>
-@endforeach
